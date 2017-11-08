2e252fecb4d4bef3d9491039060dbf662cfc6e36
Chris M <webmaster@magnaws.com>
2017-11-08 16:20:43 -0500

    Removed the stupid MonsterTown project (I didn't realize I checked it in), and adds a playground for understanding default parameters.

---
+++ MonsterTown/MonsterTown/Zombie.swift
@@ -0,0 +1,9 @@
+//
+//  Zombie.swift
+//  MonsterTown
+//
+//  Created by Chris Marshall on 11/7/17.
+//  Copyright © 2017 BNR. All rights reserved.
+//
+
+import Foundation
---
+++ MonsterTown/MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Chris Marshall on 11/7/17.
+//  Copyright © 2017 BNR. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

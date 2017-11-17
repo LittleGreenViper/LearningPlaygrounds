a67b54880cd2b1845cae12099d83eee84d0c6bfa
Chris M <webmaster@magnaws.com>
2017-11-17 16:07:44 -0500

    Bunch of work on iterators and sequences.

--- RangesAndStrides.playground/Contents.swift
+++ RangesAndStrides.playground/Contents.swift
@@ -10,6 +10,9 @@ let closedRangeIsEmpty = (0...0).isEmpty
 //: Same here (which makes sense).
 //let downwardSpiral = 1..<0
 
+let onwardAndUpward1 = -10..<10 // This is a half-open Range, from -10 to &lt;10, in integer steps (19 steps, 20 values).
+let onwardAndUpward2 = -10...10 // This is a closed Range, from -10 to 10, in integer steps (20 steps, 21 values).
+
 //: You cannot specify Ranges backwards. This will cause an error.
 //let downwardSpiral = 1>..0
 
@@ -157,7 +160,158 @@ print(nameSort("Abby"))
 print(nameSort("ABBY"))
 //: Well, won't bother fixing it. The best way is to force case on the string, and do a simple comparison. This is really just a demo.
 
-let strider = stride(from: 10.0, to: -10.0, by: -0.25)
+let strider = stride(from: 10.0, to: -10.0, by: -3.25)
 for aragorn in strider {
     print(aragorn)
+}
+
+/*:
+ # SEQUENCES
+ 
+ This is about as simple as you'll get.
+ 
+ The data is stored internally in a Sequence (an Array of Int), so most of our work is passing the protocol into our storage Array.
+ */
+struct SimpleListy: Sequence {
+    typealias Element = Int
+    typealias Iterator = Array<Element>.Iterator
+    
+    private var _myInts:[Element] = []
+    func makeIterator() -> SimpleListy.Iterator {
+        return self._myInts.makeIterator()
+    }
+    
+    mutating func addInteger(_ inInt: Element) {
+        self._myInts.append(inInt)
+    }
+}
+
+var myIntBucket = SimpleListy()
+
+myIntBucket.addInteger(10)
+myIntBucket.addInteger(3)
+myIntBucket.addInteger(1000)
+myIntBucket.addInteger(21)
+myIntBucket.addInteger(450)
+myIntBucket.addInteger(105)
+
+print("\nPrint the integers:\n")
+for integ in myIntBucket {
+    print(integ)
+}
+
+/*:
+ The following code implements a simple sequence, where items are stored in a Dictionary, which has an unspecified sort order, but are iterated alphabetically, by first name.
+ 
+ An iterator is basically a struct or class with a "next()" method. How it gets there is why you may (or may not) want to do your own custom one.
+ 
+ This is a simple demo of why you may want a custom iterator. Say that you want an unorganized list to be iterated in an organized manner?
+ */
+//: First, we declare a crony type, with the important information.
+typealias MyLittleCrony = (name: String, favorsOwed: Int, isRelative: Bool)
+
+//: Next, we declare a Sequence struct, which will include a custom ordered iterator.
+struct CronyList: Sequence {
+    //: This is a typealias for our internal Dictionary
+    typealias CronyListDictionary = [String:Element]
+    //: This is required by the protocol
+    typealias Element = MyLittleCrony
+    //: This is our internal opaque storage.
+    private var _cronyList: CronyListDictionary = [:]
+    
+    //: This is the iterator we'll use. Note that it sorts the response, using the Dictionary keys.
+    struct Iterator : IteratorProtocol {
+        //: We capture a copy of the list here.
+        private let _iteratorList:CronyListDictionary
+        //: This is an array of Dictionary keys that we use to sort.
+        private let _keysArray:[String]
+        //: This is the current item index.
+        private var _index: Int
+        
+        //: Just capture the main object at the time the iterator is made.
+        init(_ myLittleCronies: CronyListDictionary) {
+            self._iteratorList = myLittleCronies
+            self._keysArray = self._iteratorList.keys.sorted()    // This sorts the iteration
+            self._index = 0
+        }
+        
+        //: This is required by the protocol.
+        mutating func next() -> Element? {
+            //: We use the sorted keys array to extract our response.
+            if self._index < self._keysArray.count {
+                let ret = self._iteratorList[self._keysArray[self._index]]
+                
+                self._index += 1
+                
+                return ret
+            } else {
+                return nil
+            }
+        }
+    }
+    
+    //: This is required by the protocol.
+    func makeIterator() -> CronyList.Iterator {
+        return Iterator(self._cronyList)
+    }
+    
+    //: We are legion
+    var count: Int {
+        get {
+            return self._cronyList.count
+        }
+    }
+
+    //: This is simply a convenient way to cast our random Dictionary into an ordered Array.
+    var cronies: [Element] {
+        get {
+            var ret: [Element] = []
+            
+            //: We use the iterator to order the Array.
+            for crony in self {
+                ret.append(crony)
+            }
+            
+            return ret
+        }
+    }
+    
+    //: This allows us to make believe we're an Array.
+    subscript(_ index: Int) -> Element {
+        return self.cronies[index]
+    }
+    
+    //: This is just how we'll load up the list.
+    mutating func append(_ crony: Element) {
+        self._cronyList[crony.name] = crony
+    }
+}
+
+var myCronies = CronyList()
+
+myCronies.append((name: "Babs", favorsOwed: 7, isRelative: true))
+myCronies.append((name: "Zulinda", favorsOwed: 10, isRelative: true))
+myCronies.append((name: "Adriaaaaaan!", favorsOwed: 3, isRelative: false))
+myCronies.append((name: "Sue", favorsOwed: 14, isRelative: true))
+myCronies.append((name: "Cross-Eyed Mary", favorsOwed: 14, isRelative: false))
+myCronies.append((name: "Charmain", favorsOwed: 14, isRelative: false))
+myCronies.append((name: "Lucrecia McEvil", favorsOwed: 2, isRelative: false))
+
+print("\nWe have \(myCronies.count) cronies.\n")
+print("\nFirst, this is the order in which the entries are stored in the Dictionary:\n")
+print(myCronies)
+
+print("\nNext, we iterate the Dictionary, using our custom iterator, which sorts alphabetically:\n")
+for (name, favorsOwed, isRelative) in myCronies {
+    print("\(name) is owed \(favorsOwed) favors, and is \(isRelative ? "" : "not ")family.")
+}
+
+print("\nNext, we show how our accessor has sorted the contents of the Dictionary for us:\n")
+print(myCronies.cronies)
+
+print("\nFinally, we show how we can subscript our Sequence directly, and get it in order:\n")
+
+for index in 0..<myCronies.count {
+    let (name, favorsOwed, isRelative) = myCronies[index]
+    print("myCronies[\(index)]:\(name) is owed \(favorsOwed) favors, and is \(isRelative ? "" : "not ")family.")
 }
